6fba984fa037268042cca2111e62a20bafb72943
Andy Cheung <77382250+andy3278@users.noreply.github.com>
2021-10-16 19:01:48 +0800

    applied theme ououe

---
+++ dev/docs/.vuepress/config.js
@@ -0,0 +1,56 @@
+module.exports = {
+    title: "Andy's Blog",
+    description: 'This is my blog built with vuepress!',
+    base: '/',
+    theme: 'ououe',
+    themeConfig: {
+        cover: {
+            base: '/cover.jpg',
+            //'/posts/': '/posts.jpg'
+            // ...
+        },
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Blogs', link: '/blogs/' },
+            { text: 'About', link: '/about/' }
+        ],
+
+        footer: [
+            { text: 'Github', link: 'https://github.com/andy3278' }
+        ],
+    },
+    postcss: {
+        plugins: [
+            require('css-prefers-color-scheme/postcss'),
+            require('autoprefixer')
+        ]
+    },
+    plugins: [
+        // permalink for posts
+        ['blog-multidir', {
+        postsDir: {
+            posts: 'posts/:year/:month/:day/:slug'
+        }
+        }],
+        // add vuepress-plugin-container
+        ['container', {
+            type: 'right',
+            defaultTitle: ''
+        }],
+        ['container', {
+            type: 'tip',
+            before: info => `<div class="tip"><p class="title">${info}</p>`,
+            after: '</div>'
+        }],
+        ['container', {
+            type: 'warning',
+            before: info => `<div class="warning"><p class="title">${info}</p>`,
+            after: '</div>'
+        }],
+        ['container', {
+            type: 'danger',
+            before: info => `<div class="danger"><p class="title">${info}</p>`,
+            after: '</div>'
+        }]
+    ]
+}
---
+++ dev/docs/blogs/test.md
@@ -0,0 +1,14 @@
+---
+title: How to use
+display: home
+image: ...
+date: 2019-02-22
+tags:
+  - vuepress
+  - vuepress-themt-ououe
+categories:
+  - blogs
+  - theme
+tag: vuepress
+category: blogs
+---
